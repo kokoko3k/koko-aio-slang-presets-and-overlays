0ff8cb8063a2c90a9aaa3495549c71574dfed4f0
Antonio Orefice <kokoko3k@gmail.com>
2023-10-04 11:13:36 +0200

    Add immersive presets for Duimon textures

--- Duimon/10-bulk_convert.sh
+++ Duimon/10-bulk_convert.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-presets="monitor-slotmask-bloom-bezel tv-aperturegrille-bloom-bezel"
+# Start me from the directory containing "textures" subdir.
+
+presets_reference_dir='Presets-ng\/'
+#presets="monitor-slotmask-bloom-bezel tv-aperturegrille-bloom-bezel"
+presets="Monitor-Ambilight-immersive"
 
 preset_prefix="preset"
 
@@ -10,9 +14,10 @@ for preset in $presets ; do
 		texturename=$(basename $f)
 		echo $preset/$texturename
 		
+		
 		cat 20-template_preset.txt | \
 			sed "s/replacemetexture/textures\/$texturename/" | \
-			sed "s/replacemeslangp/$preset.slangp/" \
+			sed "s/replacemeslangp/$presets_reference_dir$preset.slangp/" \
 				> "$preset_prefix"_"$preset"/"${texturename%.*}".slangp 
 
 	done
